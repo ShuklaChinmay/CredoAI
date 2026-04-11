5f9abe5646d41267c8eb998b63063416f1342ff0
ChinmayShukla04 <chinmayshukla04@gmail.com>
2026-04-12 04:16:01 +0530

    "updated the authentication"

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,22 +1,25 @@
-import axios from "axios";
+// import axios from "axios";
 
-const API = "http://127.0.0.1:8000";
+import api from "./api"; 
+
+
+import api from "./api"; // 👈 use common axios instance
 
 const authService = {
   login: (email, password) =>
-    axios.post(`${API}/auth/login`, { email, password }),
+    api.post(`/auth/login`, { email, password }),
 
   register: (data) =>
-    axios.post(`${API}/auth/register`, data),
+    api.post(`/auth/register`, data),
 
   verifyOtp: (email, otp) =>
-    axios.post(`${API}/auth/verify-otp`, { email, otp }),
+    api.post(`/auth/verify-otp`, { email, otp }),
 
   forgotPassword: (email) =>
-    axios.post(`${API}/auth/forgot-password`, { email }),
+    api.post(`/auth/forgot-password`, { email }),
 
   resetPassword: (email, otp, new_password) =>
-    axios.post(`${API}/auth/reset-password`, {
+    api.post(`/auth/reset-password`, {
       email,
       otp,
       new_password,
